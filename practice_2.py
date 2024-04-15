7dc7cdf1c5446b7650ebaf3679d08f6b9a4d2734
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-15 16:07:35 +0700

    solve practice 2

--- practice_2.py
+++ practice_2.py
@@ -4,7 +4,7 @@ def print_the_sum(n):
     for num in range(n):
         sum = previous_num + num
         print("Current number", num, "Previous number", previous_num, "Sum", sum)
-        #sum = previous_num + num
+        sum = previous_num + num
         previous_num = num
         
 n = int(input("Input the range: "))
