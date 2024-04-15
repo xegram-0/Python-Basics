8c266fac841c853f06705d8cf525e9abcd525b98
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-15 12:24:55 +0700

    added problem

--- practice_1.py
+++ practice_1.py
@@ -1,2 +1,9 @@
-print("hello world")
-print("test")
+#print("hello world")
+#print("test")
+# Exercise 1: Calculate the multiplication and sum of two numbers
+a = int(input("Number 1: "))
+b = int(input("Number 2: "))
+if (a*b)<1000:
+    print("The result is", a*b) 
+else:
+    print("The result is", a+b)
