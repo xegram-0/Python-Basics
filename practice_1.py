0665a0ec32354b64c9af4f10914fde6a7a9e82c5
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-15 12:12:00 +0700

    added practice 1 problem

--- practice_1.py
+++ practice_1.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("hello world")
 print("test")
-=======
-print("hello world")
->>>>>>> f010ee8 (added practice_1)
-=======
-print("hello world")
-print("test")
->>>>>>> 33b10ee (added test)
