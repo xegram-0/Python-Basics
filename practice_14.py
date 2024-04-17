2225de17c981cbc313e22008d2b7f42c8b2f755f
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-17 17:31:11 +0700

    solve partially practice 14

---
+++ practice_14.py
@@ -0,0 +1,15 @@
+# Print a downward Half-Pyramid 
+# Pattern of Star (asterisk)
+# * * * * *  
+# * * * *  
+# * * *  
+# * *  
+# *
+
+num = int(input("How many rows: "))
+# pay attention to this
+for i in range(num+1,0,-1):
+    for j in range(i-1):
+        print(j, end='')
+        print('*' ,end=' ')
+    print("\t")
