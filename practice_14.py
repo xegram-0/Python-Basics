513364391898bc3367c8c66ea6969afeeac33019
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-17 21:33:34 +0700

    mark a comment

--- practice_14.py
+++ practice_14.py
@@ -10,6 +10,6 @@ num = int(input("How many rows: "))
 # pay attention to this
 for i in range(num+1,0,-1):
     for j in range(i-1):
-        print(j, end='')
+        #print(j, end='')
         print('*' ,end=' ')
     print("\t")
